58615f51ec25e622b96c96082d18f58bef6b72b7
Yasunori Shimura <yasu0710@gmail.com>
2022-08-13 18:52:10 +0900

    Added some comments in the source

--- include/fdc_bitstream.h
+++ include/fdc_bitstream.h
@@ -79,12 +79,22 @@ public:
     inline bool is_wraparound(void) { return m_codec.is_wraparound(); }
     inline void clear_wraparound(void) { m_codec.clear_wraparound(); }
 
-    /** Enable FDC read operation fluctuatior */
+    /** Enable FDC read operation fluctuatior
+     * If you set the fluctuator with enable_fluctuator(1,4), the VFO in the data separator will operate at 
+     * rate of 3/4 and stop operation at the rate of 1/4. The operation will be determined by 
+     a random generator, so the VFO operation will be stochastic, and this introduces some uncertainty in the read data.
+     */
     inline void enable_fluctuator(size_t numerator, size_t denominator) { m_codec.enable_fluctuator(numerator, denominator); }
     /** Disable FDC read operation fluctuator */
     inline void disable_fluctuator(void) { m_codec.disable_fluctuator(); }
 
-    /** Set VFO gain */
+    /** Set VFO gain
+    * Recommended setting : High gain (high speed) mode(10.0f). Low gain (1.0f) for data reading. 
+    * FDC needs to synchronized with the data stream quickly in the SYNC field before start reading the actual data (sector ID or sector body).
+    * High speed (high gain) setting will be used during the SYNC field to lock-in the read timing quickly.
+    * Low speed (low gain) setting will be used during reading data (other than SYNC field) to cancel slow spindle speed 
+    * fluctuation but not follow the quick timing change such as irregular pulses in the bit stream and keep the same reading pace.
+    */
     inline void set_vfo_gain(double low, double high) { m_codec.set_vfo_gain(low, high); }
 
     void write_data(uint8_t data, bool mode = false, bool write_gate = true);
