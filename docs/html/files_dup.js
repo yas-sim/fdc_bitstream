8d9c07c64728119ab0b2b938c5a48b1cb190a025
yas-sim <yas-sim@users.noreply.github.com>
2022-08-16 03:52:22 +0000

    Update docs

--- docs/html/files_dup.js
+++ docs/html/files_dup.js
@@ -1,6 +1,5 @@
 var files_dup =
 [
-    [ "check_mfm_image", "dir_359e0e1dfe6122d678b82d1a2a8059d1.html", "dir_359e0e1dfe6122d678b82d1a2a8059d1" ],
     [ "create_mfm_image", "dir_b69692e95baa5c641c3b74eda32f3092.html", "dir_b69692e95baa5c641c3b74eda32f3092" ],
     [ "fdc_bitstream", "dir_04700cb65f3786cf311fa988e0d45468.html", "dir_04700cb65f3786cf311fa988e0d45468" ],
     [ "fdc_test", "dir_1cd538333abeb00f0c1f53741fd76805.html", "dir_1cd538333abeb00f0c1f53741fd76805" ],
--- docs/html/search/all_e.js
+++ docs/html/search/all_e.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['offset_0',['offset',['../structpictrack__.html#a568b91983596d07addf3fe543a27765c',1,'pictrack_::offset()'],['../structtrack__table__.html#a72e86cb27f462ddf0880fa65fc5a9131',1,'track_table_::offset()']]],
   ['open_1',['open',['../classkfx2mfm_1_1_k_f_x__stream.html#ac1cceba240a1233c8b7f72ef0a25e9bc',1,'kfx2mfm::KFX_stream']]],
-  ['open_5fbinary_5ffile_2',['open_binary_file',['../classdisk__image.html#a4fdc06a7d330106b3432c70c3da4ab52',1,'disk_image']]]
+  ['open_5fbinary_5ffile_2',['open_binary_file',['../classdisk__image.html#a4fdc06a7d330106b3432c70c3da4ab52',1,'disk_image']]],
+  ['open_5ftext_5ffile_3',['open_text_file',['../classdisk__image.html#aa2fbdcb556ce5ac6f9970ed65945a815',1,'disk_image']]]
 ];
