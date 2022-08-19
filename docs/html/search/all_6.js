5118a7eaf4405efecaec8d461e060eae930f55ae
yas-sim <yas-sim@users.noreply.github.com>
2022-08-19 11:27:28 +0000

    Update docs

--- docs/html/search/all_6.js
+++ docs/html/search/all_6.js
@@ -15,11 +15,12 @@ var searchData=
   ['fdc_5ftest_2ecpp_12',['fdc_test.cpp',['../fdc__test_8cpp.html',1,'']]],
   ['fill_13',['fill',['../classbyte__array.html#a1eb63bbeb65665d3744493cb6868d4bd',1,'byte_array::fill(size_t pos, size_t count, uint8_t val)'],['../classbyte__array.html#a1eb63bbeb65665d3744493cb6868d4bd',1,'byte_array::fill(size_t pos, size_t count, uint8_t val)']]],
   ['fill_5fstream_14',['fill_stream',['../classbit__array.html#a36cdaff5e771ccbe45e63a691730ba12',1,'bit_array']]],
-  ['floppy_5f2d_15',['FLOPPY_2D',['../fdc__defs_8h.html#a99c5ff7e5199a13ca4884062b413d68fa048b9b8231a2e07d36aed7acd46a0cd0',1,'fdc_defs.h']]],
-  ['floppy_5f2dd_16',['FLOPPY_2DD',['../fdc__defs_8h.html#a99c5ff7e5199a13ca4884062b413d68fa3a51151130f155d99f8482dccae69e7a',1,'fdc_defs.h']]],
-  ['floppy_5f2hd_17',['FLOPPY_2HD',['../fdc__defs_8h.html#a99c5ff7e5199a13ca4884062b413d68faadc6bc39a38b1133be70a07ec60b6faf',1,'fdc_defs.h']]],
-  ['floppyinterfacemode_18',['floppyinterfacemode',['../structpicfileformatheader__.html#ac5a89ec543644ddcceb7b329459c7d20',1,'picfileformatheader_']]],
-  ['floppyinterfacemode_5ft_19',['floppyinterfacemode_t',['../image__hfe_8h.html#a18093199b529fa24559846bd4e561fdf',1,'image_hfe.h']]],
-  ['floppyrpm_20',['floppyRPM',['../structpicfileformatheader__.html#aca6a30f39b4723fadbb8d114b9f3feee',1,'picfileformatheader_']]],
-  ['formatrevision_21',['formatrevision',['../structpicfileformatheader__.html#a6553abb8999b3fbe770a74ae9d7dfbfb',1,'picfileformatheader_']]]
+  ['float_15',['float',['../namespacekfx2mfm.html#a5bf69425fee987e13c7a6c356784b89d',1,'kfx2mfm']]],
+  ['floppy_5f2d_16',['FLOPPY_2D',['../fdc__defs_8h.html#a99c5ff7e5199a13ca4884062b413d68fa048b9b8231a2e07d36aed7acd46a0cd0',1,'fdc_defs.h']]],
+  ['floppy_5f2dd_17',['FLOPPY_2DD',['../fdc__defs_8h.html#a99c5ff7e5199a13ca4884062b413d68fa3a51151130f155d99f8482dccae69e7a',1,'fdc_defs.h']]],
+  ['floppy_5f2hd_18',['FLOPPY_2HD',['../fdc__defs_8h.html#a99c5ff7e5199a13ca4884062b413d68faadc6bc39a38b1133be70a07ec60b6faf',1,'fdc_defs.h']]],
+  ['floppyinterfacemode_19',['floppyinterfacemode',['../structpicfileformatheader__.html#ac5a89ec543644ddcceb7b329459c7d20',1,'picfileformatheader_']]],
+  ['floppyinterfacemode_5ft_20',['floppyinterfacemode_t',['../image__hfe_8h.html#a18093199b529fa24559846bd4e561fdf',1,'image_hfe.h']]],
+  ['floppyrpm_21',['floppyRPM',['../structpicfileformatheader__.html#aca6a30f39b4723fadbb8d114b9f3feee',1,'picfileformatheader_']]],
+  ['formatrevision_22',['formatrevision',['../structpicfileformatheader__.html#a6553abb8999b3fbe770a74ae9d7dfbfb',1,'picfileformatheader_']]]
 ];
--- docs/html/search/variables_8.js
+++ docs/html/search/variables_8.js
@@ -4,6 +4,5 @@ var searchData=
   ['ibmpc_5fed_5ffloppymode_1',['IBMPC_ED_FLOPPYMODE',['../image__hfe_8h.html#ab5748b28da20ada169de75d4dc612d6a',1,'image_hfe.h']]],
   ['ibmpc_5fhd_5ffloppymode_2',['IBMPC_HD_FLOPPYMODE',['../image__hfe_8h.html#a89b211557ff5baf301b1ce169f95e722',1,'image_hfe.h']]],
   ['id_5fpos_3',['id_pos',['../structfdc__bitstream_1_1sector__data.html#a2f25f01cd7afde59dd3c2283b96d6086',1,'fdc_bitstream::sector_data']]],
-  ['id_5fstr_4',['id_str',['../structmfm__header__.html#ac4589f09863bbcb3fe4137314e28ac25',1,'mfm_header_']]],
-  ['int_5',['int',['../namespacekfx2mfm.html#a740df438911e55477ee5ec87ad1bb6d4',1,'kfx2mfm']]]
+  ['id_5fstr_4',['id_str',['../structmfm__header__.html#ac4589f09863bbcb3fe4137314e28ac25',1,'mfm_header_']]]
 ];
