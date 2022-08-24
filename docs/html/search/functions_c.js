2f81b45a8cbc9d305662a59cc7d227199b648fbc
yas-sim <yas-sim@users.noreply.github.com>
2022-08-24 13:11:00 +0000

    Update docs

--- docs/html/search/functions_c.js
+++ docs/html/search/functions_c.js
@@ -1,4 +1,7 @@
 var searchData=
 [
-  ['normalize_5ftrack_0',['normalize_track',['../image__converter_8cpp.html#a60b530e94742b10ccd86f68529ae8c0c',1,'image_converter.cpp']]]
+  ['open_0',['open',['../classkfx2mfm_1_1_k_f_x__stream.html#ac1cceba240a1233c8b7f72ef0a25e9bc',1,'kfx2mfm::KFX_stream']]],
+  ['open_5fbinary_5ffile_1',['open_binary_file',['../classdisk__image.html#a4fdc06a7d330106b3432c70c3da4ab52',1,'disk_image']]],
+  ['open_5ftext_5ffile_2',['open_text_file',['../classdisk__image.html#aa2fbdcb556ce5ac6f9970ed65945a815',1,'disk_image']]],
+  ['operator_3d_3',['operator=',['../classbyte__array.html#a5a691f1423f88374a30a1f72747a664b',1,'byte_array::operator=()'],['../classdisk__image__hfe.html#aff37ad9f5d4a1608ca8d549914b48b90',1,'disk_image_hfe::operator=()'],['../classdisk__image__mfm.html#af6bf880a161cb153325b538901de0c52',1,'disk_image_mfm::operator=()'],['../classdisk__image__raw.html#a81971da1c0a98da2653eb6e299b06115',1,'disk_image_raw::operator=()']]]
 ];
