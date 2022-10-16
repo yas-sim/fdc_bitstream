0ee3f7dc179f029166709480c0ca30b7be6cf614
Soji Yamakawa <soji@andrew.cmu.edu>
2022-10-16 17:29:53 -0400

    Adding image-comparison tests.

---
+++ tests/compare.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <string>
+#include <memory>
+#include "util.h"
+
+
+int main(int ac,char *av[])
+{
+	int fNameCount=0;
+	std::string fName[2];
+	bool ignoreCRCErrorSectors=false;
+
+	for(int i=1; i<ac; ++i)
+	{
+		if("-ignore_crc"==av[i])
+		{
+			ignoreCRCErrorSectors=true;
+		}
+		else if(fNameCount<2)
+		{
+			fName[fNameCount++]=av[i];
+		}
+		else
+		{
+			std::cout << "Undefined option: " << av[i] << std::endl;
+			return 1;
+		}
+	}
+
+	if(2!=fNameCount)
+	{
+		std::cout << "Two file names must be given." << std::endl;
+		return 1;
+	}
+
+	std::unique_ptr <disk_image> img[2]={nullptr,nullptr};
+
+	for(int i=0; i<2; ++i)
+	{
+		size_t lastDot=0;
+		for(size_t j=0; j<fName[i].size(); ++j)
+		{
+			if(fName[i][j]=='.')
+			{
+				lastDot=j;
+			}
+		}
+		std::string ext=fName[i].substr(lastDot);
+		for(auto &c : ext)
+		{
+			if('a'<=c && c<='z')
+			{
+				c=c+'A'-'a';
+			}
+		}
+		if(".RAW"==ext)
+		{
+			img[i]=std::unique_ptr<disk_image>(new disk_image_raw);
+		}
+		if(".MFM"==ext)
+		{
+			img[i]=std::unique_ptr<disk_image>(new disk_image_mfm);
+		}
+		if(".FDX"==ext)
+		{
+			img[i]=std::unique_ptr<disk_image>(new disk_image_fdx);
+		}
+		if(".D77"==ext)
+		{
+			img[i]=std::unique_ptr<disk_image>(new disk_image_d77);
+		}
+		if(".HFE"==ext)
+		{
+			img[i]=std::unique_ptr<disk_image>(new disk_image_hfe);
+		}
+
+		if(nullptr==img[i])
+		{
+			std::cout << "Unsupported file type." << std::endl;
+			return 1;
+		}
+
+		img[i]->read(fName[i]);
+		if(true!=img[i]->is_ready())
+		{
+			std::cout << "Failed to read image." << std::endl;
+			return 1;
+		}
+	}
+
+	std::cout << "Images Loaded." << std::endl;
+	if(true!=CompareDisk(*img[0],*img[1],fName[0],fName[1],ignoreCRCErrorSectors))
+	{
+		std::cout << "Comparison Failed." << std::endl;
+		return 1;
+	}
+
+	std::cout << "Pass!" << std::endl;
+
+	return 0;
+}
--- tests/raw2d77.cpp
+++ tests/raw2d77.cpp
@@ -2,6 +2,7 @@
 
 #include "disk_images.h"
 #include "fdc_bitstream.h"
+#include "util.h"
 
 
 
@@ -171,98 +172,6 @@ unsigned char trueSectorData[]=
 0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,
 };
 
-
-bool CompareDisk(const disk_image &diskA,const disk_image &diskB,std::string diskALabel,std::string diskBLabel)
-{
-	std::cout << "Compare " << diskALabel << " and " << diskBLabel << std::endl;
-
-	if(diskA.get_number_of_tracks()!=diskB.get_number_of_tracks())
-	{
-		std::cout << "Track count do not match." << std::endl;
-		std::cout << "    " << diskA.get_number_of_tracks() << std::endl;
-		std::cout << "    " << diskB.get_number_of_tracks() << std::endl;
-		return false;
-	}
-	for(unsigned int trk=0; trk<diskA.get_number_of_tracks(); ++trk)
-	{
-		fdc_bitstream drive[2];
-
-		drive[0].set_track_data(diskA.get_track_data(trk));
-		drive[0].set_pos(0);
-		drive[0].set_vfo_type(VFO_TYPE_DEFAULT);
-		drive[0].set_vfo_gain_val(VFO_GAIN_L_DEFAULT,VFO_GAIN_H_DEFAULT);
-		auto idDisk0=drive[0].read_all_idam();
-
-		drive[1].set_track_data(diskB.get_track_data(trk));
-		drive[1].set_pos(0);
-		drive[1].set_vfo_type(VFO_TYPE_DEFAULT);
-		drive[1].set_vfo_gain_val(VFO_GAIN_L_DEFAULT,VFO_GAIN_H_DEFAULT);
-		auto idDisk1=drive[0].read_all_idam();
-
-		if(idDisk0.size()!=idDisk1.size())
-		{
-			std::cout << "Sector count in track " << trk << " do not match." << std::endl;
-			std::cout << "    " << idDisk0.size() << std::endl;
-			std::cout << "    " << idDisk1.size() << std::endl;
-			return false;
-		}
-
-		for(auto id0 : idDisk0)
-		{
-			bool foundMatch=false;
-			for(auto id1 : idDisk1)
-			{
-				if(id0.C==id1.C &&
-				   id0.H==id1.H &&
-				   id0.R==id1.R &&
-				   id0.N==id1.N)
-				{
-					foundMatch=true;
-					break;
-				}
-			}
-			if(true!=foundMatch)
-			{
-				std::cout << "Sector (C,H,R,N)=(" << id0.C << "," << id0.H << "," << id0.R << "," << id0.N << ")" << std::endl;
-				std::cout << "in " << diskALabel << " was not found in " << diskBLabel << std::endl;
-				return false;
-			}
-		}
-
-		for(auto id : idDisk0)
-		{
-			auto sector0=drive[0].read_sector(id.C,id.R);
-			auto sector1=drive[1].read_sector(id.C,id.R);
-
-			if(true==sector0.record_not_found || true==sector1.record_not_found)
-			{
-				std::cout << "Record Not Found Error." << std::endl;
-				std::cout << "Sector (C,H,R,N)=(" << id.C << "," << id.H << "," << id.R << "," << id.N << ")" << std::endl;
-				return false;
-			}
-
-			if(sector0.data.size()!=sector1.data.size())
-			{
-				std::cout << "Sector sizes do not match." << std::endl;
-				std::cout << "Sector (C,H,R,N)=(" << id.C << "," << id.H << "," << id.R << "," << id.N << ")" << std::endl;
-				return false;
-			}
-
-			for(int i=0; i<sector0.data.size(); ++i)
-			{
-				if(sector0.data[i]!=sector1.data[i])
-				{
-					std::cout << "Sector contents do not match." << std::endl;
-					std::cout << "Sector (C,H,R,N)=(" << id.C << "," << id.H << "," << id.R << "," << id.N << ")" << std::endl;
-					return false;
-				}
-			}
-		}
-	}
-
-	return true;
-}
-
 int main(int ac,char *av[])
 {
 	if(ac<4)
@@ -294,9 +203,9 @@ int main(int ac,char *av[])
 	disk_image_d77 d77ReadBack;
 	d77ReadBack.read(tempD77output);
 
-	if(true!=CompareDisk(raw,correctD77,"RAW","Correct D77") ||
-	   true!=CompareDisk(raw,correctD77,"RAW","Converted D77") ||
-	   true!=CompareDisk(raw,d77ReadBack,"RAW","D77 Read Back"))
+	if(true!=CompareDisk(raw,correctD77,"RAW","Correct D77",false) || // false: Do not ignore CRC-error sectors.
+	   true!=CompareDisk(raw,correctD77,"RAW","Converted D77",false) ||
+	   true!=CompareDisk(raw,d77ReadBack,"RAW","D77 Read Back",false))
 	{
 		std::cout << "Error!" << std::endl;
 		return 1;
