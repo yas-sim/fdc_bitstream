53bcf48e9b89546c577c83bdd6cec3df81f23ad6
Yasunori Shimura <yasu0710@gmail.com>
2022-11-03 17:09:23 +0900

    Added -auto_trim to image_converter

--- image_converter/image_converter.cpp
+++ image_converter/image_converter.cpp
@@ -6,6 +6,7 @@
 #include <map>
 #include <unordered_map>
 #include <algorithm>
+#include <stdlib.h>
 
 #include "disk_images.h"
 
@@ -150,6 +151,160 @@ void inspect_track(std::vector<bit_array> &track_data, std::vector<std::unordere
     }
 }
 
+/**
+ * @brief 
+ * Returns AM positions in MFM byte position
+ * @param mfm_data 
+ * @param mc_data 
+ * @param mfm_pos 
+ * @return std::vector<size_t> 
+ */
+std::vector<size_t> find_address_marks(std::vector<size_t> &mfm_data, std::vector<size_t> &mc_data, std::vector<size_t> &mfm_pos) {
+    std::vector<size_t> res;
+    size_t pos = 0;
+    size_t prev_mc = 0;
+    while(pos < mfm_data.size()) {
+        if(prev_mc==1 && mc_data[pos]==0) {
+            if((mfm_data[pos] & 0xfc) >= 0xfc) {      // found an address mark field
+                res.push_back(pos);
+            }
+        }
+        prev_mc = mc_data[pos];
+        pos++;
+    }
+    return res;
+}
+
+/**
+ * @brief Get the pulse dist data object
+ * Creates and returns a list of pulse distance data
+ * @param track_data 
+ * @param start_pos 
+ * @param num 
+ * @return std::vector<size_t> 
+ */
+std::vector<size_t> get_pulse_dist_buf(bit_array &track_data, size_t start_pos, size_t num = (16*16)) {
+    std::vector<size_t> res;
+    track_data.set_stream_pos(start_pos);
+    for(size_t i=0; i<num; i++) {
+        res.push_back(track_data.distance_to_next_pulse());
+    }
+    return res;
+}
+
+double calc_dist_buf_correlation(std::vector<size_t> dist_buf0, std::vector<size_t> dist_buf1) {
+    size_t len = std::min(dist_buf0.size(), dist_buf1.size());
+    double metric = 0.f;
+    for(size_t i = 0; i < len; i++) {
+        size_t diff = std::abs(static_cast<int>(dist_buf0[i]) - static_cast<int>(dist_buf1[i]));
+        metric += diff * diff;             // SSD
+        //metric += diff;                    // SAD
+    }
+    return metric;
+}
+
+size_t compare_data(std::vector<size_t> data, size_t pos1, size_t pos2, size_t size = 0x10) {
+    size_t error = 0;
+    for(size_t ofst = 0; ofst < size; ofst++) {
+        error += std::abs(static_cast<int>(data[pos1 + ofst]) - static_cast<int>(data[pos2 + ofst]));
+    }
+    return error;
+}
+
+void dump(std::vector<size_t> data, size_t pos, size_t size = 0x10) {
+    std::cout << std::hex << std::setw(10) << std::setfill('0') << pos << " : ";
+    for(size_t i = 0; i < size; i++) {
+        std::cout << std::hex << std::setw(2) << std::setfill('0') << data[pos + i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+/**
+ * @brief 
+ * Trim track data with read-overlap to connect the top of the track data and the end of the track data nicely and smoothly.
+ * 
+ * @param track_data 
+ * @return bit_array 
+ */
+void stitch_track_auto(disk_image *input_image, bool verbose) {
+    disk_image_base_properties prop = input_image->get_property();
+    size_t pos_top_of_2nd_spin = (prop.m_spindle_time_ns) / (1e9 / prop.m_sampling_rate);
+
+    fdc_bitstream fdc;
+    std::vector<std::vector<size_t>> read_data; // (mfm_data, mc_flag, pos, error)
+
+    for(size_t track_num = 0; track_num < prop.m_number_of_tracks; track_num++) {
+        bit_array track_data = input_image->get_track_data(track_num);
+        fdc.set_track_data(track_data);
+        fdc.set_pos(0);
+        read_data = fdc.read_track_ex();                                  // perform read track
+        std::vector<size_t> mfm_data, mc_data, mfm_pos;
+        for(auto it = read_data.begin(); it != read_data.end(); it++) {   // separate mfm_data, mc_data, and mfm_pos
+            mfm_data.push_back((*it)[0]);
+            mc_data.push_back((*it)[1]);
+            mfm_pos.push_back((*it)[2]);
+        }
+        std::vector<size_t> am_pos = find_address_marks(mfm_data, mc_data, mfm_pos);
+
+        size_t trim_pos = pos_top_of_2nd_spin;              // default trim position (in bit pos). will applied if no pair of match AM field is detected.
+        if(am_pos.size() != 0) {                            // AM fields detected
+            size_t i = 0;
+            for(i=0; i < am_pos.size(); i++) {
+                if(mfm_pos[am_pos[i]] >= pos_top_of_2nd_spin) break;                         // find the 1st AM in the 2nd spin
+            }
+            if(i != am_pos.size()) {                                                         // AM fields found in the 2nd spin
+                std::vector<size_t> am_pos_1s { am_pos.begin()    , am_pos.begin() + i - 1};
+                std::vector<size_t> am_pos_2s { am_pos.begin() + i, am_pos.end()};
+                size_t i1 = 0, i2 = 0;
+                bool match_flag = true;
+                while(compare_data(mfm_data, am_pos_1s[i1], am_pos_2s[i2], 0x10) != 0) {      // find a match AM pair from the 1st spin and the 2nd spin (compare 0x10 bytes)
+                    if(++i1 == am_pos_1s.size()) {
+                        i1 = 0;
+                        if(++i2 == am_pos_2s.size()) {
+                            match_flag = false;
+                            break;
+                        }
+                    }
+                }
+                if(match_flag) {       // Found match AM pair.
+                    size_t offset_2s_mfm = am_pos_1s[i1];                           // offset (in MFM byte count) to the top of the 2nd spin.
+                    size_t top_pos_2s = mfm_pos[am_pos_2s[i2] - offset_2s_mfm];      // position (in bit_array pos) of the top of the 2nd spin.
+                    if(verbose) {
+                        std::cout << "TRACK " << std::dec << track_num << std::endl; 
+                        dump(mfm_data, am_pos_1s[i1]);
+                        dump(mfm_data, am_pos_2s[i2]);
+                    }
+                    //dump(mfm_data, 0);
+                    //dump(mfm_data, am_pos_2s[i2] - offset_2s_mfm);
+                    // Search precise trim point
+                    const size_t compare_length = 128;
+                    std::vector<size_t> dist_buf0 = get_pulse_dist_buf(track_data, 0, compare_length);           // distance buffer for 128 pulses from the top of the track
+                    std::vector<size_t> dist_buf1;
+                    int bit_cell_size = prop.m_sampling_rate / prop.m_data_bit_rate;
+                    double min_error = 1e99;
+                    int min_offset = 0;
+                    for(int offset = -(bit_cell_size * 32); offset < (bit_cell_size * 32); offset++) {  // search +- 4 bit cell region
+                        dist_buf1 = get_pulse_dist_buf(track_data, top_pos_2s + offset, compare_length);
+                        double error = calc_dist_buf_correlation(dist_buf0, dist_buf1);               // find least error position
+                        //std::cout << error << std::endl;
+                        if(error < min_error) {
+                            min_error = error;
+                            min_offset = offset;
+                        }
+                    }
+                    if(verbose) {
+                        std::cout << "Min error:" << std::dec << min_error << ", offset:" << min_offset << std::endl;
+                        std::cout << std::endl;
+                    }
+                    trim_pos = top_pos_2s + min_offset;      // adjust the trim position
+                }
+            }
+        }
+        track_data.resize(trim_pos);                // perform track data trimming
+        input_image->set_track_data(track_num, track_data);    // replace track data with trimmed one
+    }
+}
+
 int main(int argc, char* argv[]) {
 
 	std::vector<std::string> cmd_opts;
@@ -165,6 +320,7 @@ int main(int argc, char* argv[]) {
     double gain_h = VFO_GAIN_H_DEFAULT;
     bool verbose = false;
     bool raw_flag = false;          // effective only for FDX format
+    bool auto_trim = false;         // auto trim track
 
 	for(auto it = cmd_opts.begin(); it != cmd_opts.end(); ++it) {
 		if(*it == "-i" && it+1 != cmd_opts.end()) {
@@ -178,6 +334,8 @@ int main(int argc, char* argv[]) {
         } else if(*it == "-gain" && it+1 != cmd_opts.end() && it+2 != cmd_opts.end()) {
             gain_l = std::stod(*(++it));
             gain_h = std::stod(*(++it));
+        } else if(*it == "-auto_trim") {
+            auto_trim = true;
         } else if(*it == "-v") {
             verbose = true;
         } else if(*it == "-raw") {
@@ -219,6 +377,10 @@ int main(int argc, char* argv[]) {
         if(!in_images[i]->is_ready()) {
             std::cout << "Failed to read the input file. (Possibly wrong file format)" << std::endl;
             return -1;
+        }
+        if(auto_trim) {
+            if(verbose) std::cout << "Auto trim" << std::endl;
+            stitch_track_auto(in_images[i], verbose);
         }
         if(normalize) {
             disk_image_base_properties prop = in_images[i]->get_property();
